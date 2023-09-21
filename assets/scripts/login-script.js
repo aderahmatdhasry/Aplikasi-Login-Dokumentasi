a94ee9a7c9936a1fb57c6aeaecadf353c4ca5d3a
aderahmatdhasry <145152819+aderahmatdhasry@users.noreply.github.com>
2023-09-21 23:28:15 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -21,16 +21,16 @@ const inputPasswordElement = document.querySelector('#inputPassword');
 
 
 /**
-* Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
-* @constant {string}
-*/
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
 
 
 /**
-* Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
-* @constant {string}
-*/
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
 
@@ -39,18 +39,18 @@ loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
   /**
-  * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
-  * @constant {string}
-  */
+   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
 
-
   /**
-  * Membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan.
-  * @constant {string}
-  */
+   * Membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
 
+  
   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
 
